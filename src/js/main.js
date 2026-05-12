3a0e09299acb57043ff785b03571381000a528a2
Victoria Helin <vickanhelin@hotmail.com>
2026-05-12 11:05:21 +0200

    lagt till funktion som ändrar meny om inloggad

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,42 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     //formulär
     const form = document.getElementById("form");
     //cvDiv
     let cvDiv = document.getElementById("cvDiv");
+    //Ul-lista meny
+    const nav = document.getElementById("menu");
 
-    if(cvDiv) {
+    if (cvDiv) {
         getCV(cvDiv);
     }
 
-
+    changeNav(nav);
 });
 
-async function getCV(cvDiv){
+async function getCV(cvDiv) {
     //måste lägga till att hämta och skicka med token
-    try{
+    try {
         const response = await fetch('http://localhost:3000/work');
         const result = await response.json();
         result.forEach(job => {
-            cvDiv.innerHTML +=`<p>${job.companyname}</p>`;
+            cvDiv.innerHTML += `<p>${job.companyname}</p>`;
         });
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
+}
+
+function changeNav(nav) {
+
+    localStorage.setItem("cv_token", "test");
+
+    if (localStorage.getItem("cv_token")) {
+        nav.innerHTML = `
+        <li><a href="/">Hem</a></li>
+        <li><a href="/cv">Mitt CV</a></li>
+        <li><button id="logout">Logga ut</button></li>
+        `;
+    } // ska jag lägga till else med vanlig meny här? 
 }
