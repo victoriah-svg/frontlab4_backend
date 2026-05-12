22774e8d05d260efd1d867eb7d64b296dc1ea625
Victoria Helin <vickanhelin@hotmail.com>
2026-05-12 12:04:48 +0200

    gjort funktion för att logga in användare

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,22 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
     //formulär
-    const form = document.getElementById("form");
+    const form = document.getElementById("loginForm");
     //cvDiv
     let cvDiv = document.getElementById("cvDiv");
     //Ul-lista meny
     const nav = document.getElementById("menu");
 
+     changeNav(nav);
+
     if (cvDiv) {
         getCV(cvDiv);
     }
 
-    changeNav(nav);
+    if(loginForm){
+        loginForm.addEventListener("submit", loginUser);
+    }
+   
 });
 
 async function getCV(cvDiv) {
@@ -56,5 +61,50 @@ function changeNav(nav) {
             localStorage.removeItem("cv_token");
             window.location.href = "login.html";
         });
+    }
+}
+
+async function loginUser(e) {
+    e.preventDefault();
+
+    let usernameInput = document.getElementById("username").value;
+    let passwordInput = document.getElementById("password").value;
+
+    //Kontroll att data är lagrad i fälten 
+    if(!usernameInput || !passwordInput){
+        console.log("Fyll i alla fält"); //skriv ut felmeddelande
+        return 
+    }
+
+    //Skapa objekt för användare 
+    let user = {
+        username: usernameInput,
+        password: passwordInput
+    }
+
+    try{
+        const response = await fetch("http://localhost:3000/authAPI/login",
+            {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(user)
+            }
+        );
+
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            //lagrar token som skickats med från anropet i localStorage
+            localStorage.setItem("cv_token", data.token);
+            //Dirigerar om till cv-sidan
+            window.location.href = "cv.html";
+        }else{
+            throw error;
+        }
+
+    }catch(error){
+        console.log("Felaktigt användarnamn eller lösenord"); //Skriv ut till DOM istället
     }
 }
